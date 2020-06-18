3a92b50a8e9690e734893f296071da840f9e931e
caicindy87 <caicindy87@gmail.com>
2020-06-18 10:54:19 -0700

    Added profile_pic attribute to UserSerializer so front-end can use the post's owner's profile pic

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -26,11 +26,13 @@ class ApplicationController < ActionController::API
   end
 
   def current_user
+    
     user ||= User.find_by(id: user_id)
   end
 
   def logged_in?
     !!current_user
+    
   end
 
   def authorized
