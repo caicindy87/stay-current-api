53e930d5f42e8890d147a33d1815d8302268362b
caicindy87 <caicindy87@gmail.com>
2020-06-30 15:48:46 -0700

    Include Rails url_helpers in post_serializer at the top of the class

--- app/serializers/post_serializer.rb
+++ app/serializers/post_serializer.rb
@@ -1,11 +1,12 @@
 class PostSerializer < ActiveModel::Serializer
+  include Rails.application.routes.url_helpers
   attributes :id, :text, :image, :upvotes, :downvotes, :created_at
   belongs_to :user
   has_many :tags, through: :post_tags
 
   def image
     if object.image.attached?
-      {url: Rails.application.routes.url_helpers.url_for(object.image), signed_id: object.image.signed_id}
+      {url: url_for(object.image), signed_id: object.image.signed_id}
     end
   end
 end
